fc0eee8b0fb86d7258700179aeddfeea8454e5a3
allexaasha-debug <allexa.asha@gmail.com>
2025-09-29 20:25:03 +0700

    Add files via upload

---
+++ passgen.py
@@ -0,0 +1,10 @@
+import random
+def gen_pass(n):
+    character = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    password = ""
+
+    for i in range(n):
+        password += random.choice(character)
+    return password
+n = int(input('masukan password:'))
+print('HASIL PASSWORD:', gen_pass(n))
